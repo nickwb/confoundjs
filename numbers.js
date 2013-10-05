840bbfeb4a2c73ff8fb96868902a6ad6ca77b65e
Nick Young <nick@nickwb.net>
2013-10-05 13:34:20 +1000

    Updated expression tree building strategy, using parameter and result objects.

--- numbers.js
+++ numbers.js
@@ -5,19 +5,24 @@ define(['underscore'], function(_) {
     var module = {};
 
     var exp = {},
-        map = {},
-        bestStrategy = {};
+        map = {};
         
     var ADD = '+', SUBTRACT = '-', MULTIPLY = '*', DIVIDE = '/';
     var operatorPrecedence = {};
     
+    // Create a map of the operator precedence
     _.each([DIVIDE, MULTIPLY, ADD, SUBTRACT], function(o, i) {
         operatorPrecedence[o] = i + 1;
     });
 
-    // In lieu of a mature class system, an inheritance mechanism for expressions
-    exp.create = function(ctor) {
-        ctor.prototype = new exp.base();
+    // A simple class system
+    var klass = function(base, ctor) {
+        if(arguments.length === 1) {
+            ctor = base;
+            base = Object;
+        }
+        
+        ctor.prototype = new base();
         
         return function() {
             var result = Object.create(ctor.prototype);
@@ -27,80 +32,155 @@ define(['underscore'], function(_) {
     };
 
     // The base expression from which all others are derived
-    exp.base = function() {
-        this.getType = function() { throw 'Must override'; }
+    exp.base = klass(function() {
         this.build = function() { throw 'Must override'; }
-        this.isAutoParenthesis = function() { return false; };
-        this.isAssociative = function() { return true; };
-        this.getOperatorPrecedence = function() { return 0; };
+    });
+    
+    var def = function(val, d) {
+        return (val !== undefined) ? val : d;
     };
+    
+    exp.buildParams = klass(function(opts) {
+        var self = this;
+        
+        this.coerce = def(opts.coerce, false);
+        this.isRoot = def(opts.isRoot, false);
+        this.depth = def(opts.depth, 0);
+        this.isBinaryOperand = def(opts.isBinaryOperand, false);
+        this.isLeftOperand = def(opts.isLeftOperand, false);
+        this.parentOperator = def(opts.parentOperator, null);
+        
+        this.createChild = function(opts) {
+            opts.depth = def(opts.depth, self.depth + 1);
+            opts.isBinaryOperand = def(opts.isBinaryOperand, self.isBinaryOperand);
+            return exp.buildParams(opts);
+        };
+    });
+    
+    exp.buildResult = klass(function() {
+        var self = this;
+        
+        this.type = null;
+        this.numeric = null;
+        this.symbolic = '';
+        this.strategy = '';
+        this.leafiness = 0;
+        this.isAtomic = false;
+        this.isAssociative = true;
+        this.operatorPrecedence = 0;
+        this.containsConcat = false;
+        this.hasParenthesis = false;
+    });
 
     // A unit expression, either 0 or 1, created by coercing
     // an array literal between Array, Number and Boolean types.
     // All expression trees are comprised of at least one unit expression.
-    exp.unit = exp.create(function(val) {
+    exp.unit = klass(exp.base, function(val) {
         this.getType = function() { return 'unit'; }
-        this.build = function(opts) {
-            opts = opts || {};
-            if(val === 0) return '+[]';
-            if(val === 1) return opts.coerce ? '+!+[]' : '!+[]';
+        this.build = function(params) {
+            if(val === 0 || val === 1) {
+                var s;
+                if(val === 0) { s = '+[]'; }
+                if(val === 1) { s = params.coerce ? '+!+[]' : '!+[]'; }
+                
+                var result = exp.buildResult();
+                result.type = 'unit';
+                result.symbolic = s;
+                result.strategy = 'unit_' + val;
+                result.numeric = val;
+                result.isAtomic = true;
+                
+                return result;
+            }
             throw 'Invalid Unit';
         };
     });
 
     // A binary expression which supports addition, subtraction, multiplication and division.
     // Most of the power of the efficiency of the number system is derived from these binary expressions.
-    exp.binary = exp.create(function(left, op, right) {
+    exp.binary = klass(exp.base, function(left, op, right) {
         
         var self = this;
         
-        this.getType = function() { return 'binary'; }
-        this.getOperatorPrecedence = function() { return operatorPrecedence[op]; };
-        this.isAssociative = function() { return op === ADD || op === MULTIPLY };
+        var myPrecedence = operatorPrecedence[op];
+        var myAssociativity = (op === ADD || op === MULTIPLY);
         
-        var doesNeedParenthesis = function(expr, isCoerced) {            
+        var doesNeedParenthesis = function(expr, params, result) {
+            // If the operand is truly atomic, it won't need any parenthesis
+            if(result.isAtomic) {
+                return false;
+            }
+            
             // Parenthesis are required if our operation is not associative,
             // or the operation at the root of the sub-tree is not associative,
             // or our operator precedence will change the meaning of the expression.
-            return  !self.isAssociative()
-                    || !expr.isAssociative(isCoerced, op)
-                    || self.getOperatorPrecedence() < expr.getOperatorPrecedence();
+            return  !myAssociativity
+                    || !result.isAssociative
+                    || myPrecedence < result.operatorPrecedence;
         };
         
-        this.build = function(opts) {
-            opts = opts || {};
+        var evaluateNumeric = function(leftOperand, rightOperand) {
+            if(op === ADD)          return leftOperand + rightOperand;
+            if(op === SUBTRACT)     return leftOperand - rightOperand;
+            if(op === MULTIPLY)     return leftOperand * rightOperand;
+            if(op === DIVIDE)       return leftOperand / rightOperand;
+        };
+        
+        var makeChildParams = function(params, isLeft) {
+            // Only the left operand needs to be explicitly coerced to a number,
+            // and only addition (as it can be confused with concatenation).     
+            var shouldCoerce = isLeft && params.coerce && op === ADD;
+            
+            return params.createChild({
+                coerce: shouldCoerce,
+                isBinaryOperand: true,
+                isLeftOperand: isLeft,
+                parentOperator: op
+            });
+        };
+        
+        this.build = function(params) {
+            
+            var binaryResult = exp.buildResult();
             
             // Map each operand if necessary
             var leftExpr = (typeof left === 'number') ? exp.map(left) : left,
                 rightExpr = (typeof left === 'number') ? exp.map(right) : right;
             
-            // Only the left operand needs to be explicitly coerced to a number,
-            // and only addition (as it can be confused with concatenation).
-            var leftCoerce = opts.coerce && (op === ADD),
-                rightCoerce = false;
+            // Create child parameters for each sub-expression
+            var leftParams = makeChildParams(params, true),
+                rightParams = makeChildParams(params, false);
+                
+            // Build each sub-expression
+            var leftResult = leftExpr.build(leftParams),
+                rightResult = rightExpr.build(rightParams);
             
             // Determine if each sub-expression requires parenthesis.
-            var leftRequiresParenthesis = doesNeedParenthesis(leftExpr, leftCoerce); 
-            var rightRequiresParenthesis = doesNeedParenthesis(rightExpr, rightCoerce);
+            var leftRequiresParenthesis = doesNeedParenthesis(leftExpr, leftParams, leftResult); 
+            var rightRequiresParenthesis = doesNeedParenthesis(rightExpr, rightParams, rightResult); 
             
-            if(leftExpr.getType() === 'unit' && rightExpr.getType() === 'concat' && op === ADD) {
+            if(leftResult.type === 'unit' && rightResult.type === 'concat' && op === ADD) {
                 leftRequiresParenthesis = true;
             }
-            
-            // Build each sub-expression
-            var leftSymbolic = leftExpr.build( { coerce: leftCoerce } ),
-                rightSymbolic = rightExpr.build( { coerce: rightCoerce } );
                 
             // Apply parenthesis if necessary
-            if(leftRequiresParenthesis && !leftExpr.isAutoParenthesis(true)) {
-                leftSymbolic = '(' + leftSymbolic + ')';
+            if(leftRequiresParenthesis && !leftResult.hasParenthesis) {
+                leftResult.symbolic = '(' + leftResult.symbolic + ')';
             }
             
-            if(rightRequiresParenthesis && !rightExpr.isAutoParenthesis(false)) {
-                rightSymbolic = '(' + rightSymbolic + ')';
+            if(rightRequiresParenthesis && !rightResult.hasParenthesis) {
+                rightResult.symbolic = '(' + rightResult.symbolic + ')';
             }
             
-            return leftSymbolic + op + rightSymbolic;
+            binaryResult.numeric = evaluateNumeric(leftResult.numeric, rightResult.numeric);
+            binaryResult.strategy = String(leftResult.numeric) + op + String(rightResult.numeric);
+            binaryResult.symbolic = leftResult.symbolic + op + rightResult.symbolic;
+            binaryResult.operatorPrecedence = myPrecedence;
+            binaryResult.isAssociative = myAssociativity;
+            binaryResult.containsConcat = leftResult.containsConcat || rightResult.containsConcat;
+            binaryResult.leafiness = 1 + Math.max(leftResult.leafiness, rightResult.leafiness);
+            
+            return binaryResult;
         };
     });
 
@@ -110,44 +190,52 @@ define(['underscore'], function(_) {
     // It is an excellent general case, capable of representing any number, provided that it has at least two digits.
     // It is also an efficient representation in many cases, although it's complexity does scale linearly with
     // the number of decimal digits in the number.
-    exp.concat = exp.create(function(val) {
-        this.isAutoParenthesis = function(isLeft) { return isLeft; };
-        this.isAssociative = function(isCoerced, outerOperator) { return !isCoerced && outerOperator !== ADD; };
-        this.getType = function() { return 'concat'; }
+    exp.concat = klass(exp.base, function(val) {
         
-        this.build = function(opts) {
-            opts = opts || {};
-            
+        this.build = function(params) {
+        
             var digits = String(val).split(''),
                 parts = [],
-                result,
+                result = exp.buildResult(),
+                childParams,
+                symbolic,
                 i;
                 
             for(i = 0; i < digits.length; i++) {
-                parts.push( '[' + exp.map( Number(digits[i]) ).build({ coerce: true }) + ']' );
+                childParams = params.createChild({ coerce: true });
+                parts.push( exp.map( Number(digits[i]) ).build(childParams) );
             }
             
-            result = '(' + parts.join('+') + ')';
-            if(opts.coerce) {
-                result = '+' + result;
+            // Build the symbolic representation
+            symbolic = _.map(parts, function(r) { return '[' + r.symbolic + ']'; });
+            symbolic = '(' + symbolic.join('+') + ')';
+            if(params.coerce) {
+                symbolic = '+' + symbolic;
             }
+            
+            
+            result.type = 'concat';
+            result.symbolic = symbolic;
+            result.numeric = val;
+            result.strategy = 'concat_' + val;
+            result.leafiness = 1 + _.reduce(parts, function(max, r) { return Math.max(r.leafiness, max); }, 0);
+            result.containsConcat = true;
+            result.isAssociative = params.isCoerced === false && params.parentOperator !== ADD;
+            result.hasParenthesis = params.isLeftOperand;
+            result.isAtomic = result.hasParenthesis;
             
             return result;
         };
     });
     
     // An expression proxy. The expression will be lazily initialised once any method is called.
-    exp.lazy = exp.create(function(val) {
+    exp.lazy = klass(exp.base, function(val) {
         var memo = null;
         var init = function() {
             !map[val] && exp.chooseBest(val);
             memo = map[val];
         };
-        
-        this.isAutoParenthesis = function(isLeft) { init(); return memo.isAutoParenthesis(isLeft);  };
-        this.isAssociative = function(isCoerced, outerOperator) { init(); return memo.isAssociative(isCoerced, outerOperator); };
-        this.getType = function() { init(); return memo.getType(); };
-        this.build = function(opts) { init(); return memo.build(opts); };
+        this.build = function(params) { init(); return memo.build(params); };
     });
     
     // Get the expression for a value from the map, or return a lazily initialised proxy if it doesn't exist.
@@ -170,19 +258,19 @@ define(['underscore'], function(_) {
         var pass = function(val) {
         
             var winner = null, smallest = Number.POSITIVE_INFINITY, count = 0;
-            var trySolution = function(name, expr) {
-                var built = expr.build({ coerce: true }),
-                    len = built.length;
+            var trySolution = function(expr) {
+                var built = expr.build(exp.buildParams({ coerce: true })),
+                    len = built.symbolic.length;
                     
                 if(len < smallest) {
                     smallest = len;
-                    winner = { name: name, expr: expr };
+                    winner = { name: built.strategy, expr: expr };
                 }
                 
                 count++;
             };
             
-            trySolution('concat', exp.concat(val));
+            trySolution(exp.concat(val));
             
             var isCandidate = function(n) {
                 n = +n;
@@ -212,8 +300,8 @@ define(['underscore'], function(_) {
                 b = val - a;
                 if(isCandidate(b)) {
                     // Sometimes the coercion is more efficient by swapping the operands
-                    trySolution(val + '=' + a + '+' + b, exp.binary(expand(a), ADD, expand(b))); 
-                    trySolution(val + '=' + b + '+' + a, exp.binary(expand(b), ADD, expand(a))); 
+                    trySolution(exp.binary(expand(a), ADD, expand(b))); 
+                    trySolution(exp.binary(expand(b), ADD, expand(a))); 
                 }
             }
             
@@ -223,7 +311,7 @@ define(['underscore'], function(_) {
                 // val = a * b
                 b = val / a;
                 if(isCandidate(b)) {
-                    trySolution(val + '=' + a + '*' + b, exp.binary(expand(a), MULTIPLY, expand(b)));
+                    trySolution(exp.binary(expand(a), MULTIPLY, expand(b)));
                 }
             }
             
@@ -234,7 +322,7 @@ define(['underscore'], function(_) {
                     // val = a - b
                     b = -(val - a);
                     if(isCandidate(b)) {
-                        trySolution(val + '=' + a + '-' + b, exp.binary(expand(a), SUBTRACT, expand(b))); 
+                        trySolution(exp.binary(expand(a), SUBTRACT, expand(b))); 
                     }
                 }
                 
@@ -244,12 +332,11 @@ define(['underscore'], function(_) {
                     // val = a / b
                     a = val * b;
                     if(isCandidate(b)) {
-                        trySolution(val + '=' + a + '/' + b, exp.binary(expand(a), DIVIDE, expand(b))); 
+                        trySolution(exp.binary(expand(a), DIVIDE, expand(b))); 
                     }
                 }
             }
             
-            bestStrategy[val] = winner.name;
             return winner.expr;
         };
         
@@ -305,14 +392,15 @@ define(['underscore'], function(_) {
     module.getSymbolic = function(val) {
         initialiseMap();
     
-        var symbolic = exp.map(val).build( { coerce: true } ),
+        var result = exp.map(val).build(exp.buildParams({ coerce: true })),
+            symbolic = result.symbolic,
             validator = new Function('return (' + symbolic + ');');
         
         if(validator() !== val) {
             throw 'Symbolic representation did not match expected value.';
         }
         
-        console.log(bestStrategy[val] || 'N/A');
+        console.log(result.strategy);
         
         return symbolic;
     };
@@ -332,12 +420,13 @@ define(['underscore'], function(_) {
         
         var total = 0;
         for(var i = 0; i <= BASE_MAP_SIZE; i++) {
-            var symbolic = exp.map(i).build( { coerce: true } );
+            var result = exp.map(i).build(exp.buildParams({ coerce: true }));
+            var symbolic = result.symbolic;
             var validator = new Function('return (' + symbolic + ');');
             var evaluated = validator();
             total += symbolic.length;
             
-            var strategy = (bestStrategy[i] || 'N/A');
+            var strategy = result.strategy;
             
             var info = padLeft(i, 3) + '=' + padRight(evaluated, 3) + ' ' + padLeft(symbolic.length, 2) + 'chars. ' + padRight(strategy, 11) + ' ' + symbolic;
             
